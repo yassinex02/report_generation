7eb0db4dd1a79af0c140bce036fec163de6116d7
yassinex02 <123518324+yassinex02@users.noreply.github.com>
2023-07-22 03:50:12 +0100

    Update table_population.py

--- table_population.py
+++ table_population.py
@@ -2,21 +2,16 @@ import pandas as pd
 import random
 from datetime import datetime, timedelta
 
-# Define the product values
 product_values = ['PB1', 'PB2']
 
-# Define the Maillon values
 maillon_values = ['Extraction', 'Manutention', 'Lavage']
 
-# Define the date range
 start_date = datetime(2022, 7, 1)
 end_date = datetime(2023, 7, 1)
 date_range = pd.date_range(start_date, end_date - timedelta(days=1), freq='D')
 
-# Create an empty DataFrame
 df = pd.DataFrame(columns=['Product', 'Date', 'Teneur PBL', 'Humidite', 'Chlore', 'Maillon'])
 
-# Generate random values for each row
 for date in date_range:
     product = random.choice(product_values)
     teneur_pbl = random.uniform(0, 1)
@@ -32,5 +27,4 @@ for date in date_range:
         'Maillon': maillon
     }, ignore_index=True)
 
-# Save the DataFrame to a CSV file
 df.to_csv('product_data.csv', index=False)
